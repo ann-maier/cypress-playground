f14d85934233e3196f187d7479606021440a6119
Anna Maier <annamaier.dev@gmail.com>
2023-09-06 13:17:45 +0200

    complete ex 6, 7, 8, 9, 10

--- cypress/integration/06-secret-menu.spec.js
+++ cypress/integration/06-secret-menu.spec.js
@@ -39,4 +39,32 @@ describe('Secret Menu Items', () => {
   it('should exist have the title on the page', () => {
     cy.get('h1').should('contain', 'Secret Menu Items');
   });
+
+  for (const property of properties) {
+    it(`should have column for ${property}`, () => {
+      cy.get(`#${property}-column`).should('be.visible');
+    });
+
+    it(`should hide ${property} column when unchecked`, () => {
+      cy.get(`#show-${property}`).uncheck();
+      cy.get(`#${property}-column`).should('not.be.visible');
+    });
+  }
+
+  for (const rating of ratings) {
+    it(`should set ${rating} and filter table data by rating`, () => {
+      cy.get('#minimum-rating-visibility').invoke('val', rating).trigger('change');
+      cy.get('td[headers="popularity-column"]').each(element => {
+        const popularity = Number(element.text());
+        expect(popularity).to.be.at.least(rating);
+      });
+    });
+  }
+
+  for (const restaurant of restaurants) {
+    it(`should set ${restaurant} and filter table data by restaurant`, () => {
+      cy.get('#restaurant-visibility-filter').select(restaurant);
+      cy.get('td[headers="whereToOrder-column"]').should('contain', restaurant);
+    });
+  }
 });
--- cypress/integration/07-echo-chamber.spec.js
+++ cypress/integration/07-echo-chamber.spec.js
@@ -9,21 +9,49 @@ describe('Initial Page', () => {
     cy.get('[data-test="application-title"]').should('contain', 'Echo Chamber');
   });
 
-  it('should have the title of the application in the window', () => {});
+  it('should have the title of the application in the window', () => {
+    cy.title().should('contain', 'Echo Chamber');
+  });
 
-  it('should navigate to "/sign-in" when you click the "Sign In" button', () => {});
+  it('should navigate to "/sign-in" when you click the "Sign In" button', () => {
+    cy.get('[data-test=sign-in]').click();
+    cy.location('pathname').should('contain', 'sign-in');
+  });
 
-  it('should navigate to "/sign-up" when you click the "Sign Up" button', () => {});
+  it('should navigate to "/sign-up" when you click the "Sign Up" button', () => {
+    cy.get('[data-test=sign-up]').click();
+    cy.location('pathname').should('contain', 'sign-up');
+  });
 });
 
 describe('Sign Up', () => {
+  const invalidEmail = 'invalid123';
+  const validEmail = 'hello@email.com';
+
   beforeEach(() => {
     cy.visit('/echo-chamber/sign-up');
+    cy.get('[data-test="sign-up-email"]').as('email');
+    cy.get('[data-test="sign-up-submit"]').as('submitButton');
   });
 
-  it('should require an email', () => {});
+  it('should require an email', () => {
+    cy.get('@submitButton').click();
+    cy.get('[data-test="sign-up-email"]:invalid').invoke('prop', 'validity').its('valueMissing').should('be.true');
+  });
 
-  it('should require that the email actually be an email address', () => {});
+  it('should require that the email actually be an email address', () => {
+    cy.get('@email').type(invalidEmail);
+    cy.get('@submitButton').click();
+    cy.get('[data-test="sign-up-email"]:invalid').invoke('prop', 'validity').its('typeMismatch').should('be.true');
 
-  it('should require a password when the email is present', () => {});
+    cy.get('@email').clear().type(validEmail);
+    cy.get('@submitButton').click();
+    cy.get('@email').invoke('prop', 'validity').its('valid').should('be.true');
+  });
+
+  it('should require a password when the email is present', () => {
+    cy.get('@email').type(validEmail);
+    cy.get('@submitButton').click();
+    cy.get('[data-test="sign-up-password"]:invalid').invoke('prop', 'validity').its('valueMissing').should('be.true');
+  });
 });
